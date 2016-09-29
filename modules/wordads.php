57b48025c6b0999945e862e3a7ede876f0d20d90
Derek Springer <derekspringer@gmail.com>
2016-09-29 15:18:12 -0700

    Module stub for future development

---
+++ modules/wordads.php
@@ -0,0 +1,10 @@
+/**
+ * Module Name: WordAds
+ * Module Description: Harness WordPress.com's advertising partners for your own website.
+ * Sort Order: ?
+ * First Introduced: 4.?
+ * Requires Connection: Yes
+ * Auto Activate: No
+ * Module Tags: TBD
+ */
+
